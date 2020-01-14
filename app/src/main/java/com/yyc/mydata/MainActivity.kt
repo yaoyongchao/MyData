d8a040c1828cbbc4649531021b06b6dbda4d9d6f
yaoyongchao <yongchao.yao@aliyun.com>
2020-01-14 17:53:28 +0800

    添加一个页面。

--- app/src/main/java/com/yyc/mydata/MainActivity.kt
+++ app/src/main/java/com/yyc/mydata/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.yyc.mydata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yyc.mydata.arry.ArrayActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * 数据结构
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
     }
+
+    private fun initViews() {
+        btn1.setOnClickListener {
+            startActivity(Intent(this,ArrayActivity::class.java))
+        }
+    }
+
 }
---
+++ app/src/main/java/com/yyc/mydata/arry/ArrayActivity.kt
@@ -0,0 +1,18 @@
+package com.yyc.mydata.arry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yyc.mydata.R
+
+class ArrayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_array)
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+}
